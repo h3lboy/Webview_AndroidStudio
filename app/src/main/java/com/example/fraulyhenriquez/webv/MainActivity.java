646ee539c93ab644f1b0907cd7c170def5567240
Frauly Henriquez <fraulyhenriquez@Fraulys-MacBook-Air.local>
2018-10-24 20:20:41 -0400

    Initial commit

---
+++ app/src/main/java/com/example/fraulyhenriquez/webv/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.fraulyhenriquez.webv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        webView = (WebView)findViewById(R.id.web_view);
+       webView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.youtube.com/channel/UCcSbUjbDjqn98_mRXIF8ReQ?view_as=subscriber");
+    }
+}
